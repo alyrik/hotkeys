ccb7dc614b2f9eb02baf9e601cc702b0bfae2329
Kiryl Anokhin <alyrik@gmail.com>
2022-04-10 14:15:18 +0200

    Upgrade react-query to v4

--- actions/queries/hooks/useGetIndividualResults.ts
+++ actions/queries/hooks/useGetIndividualResults.ts
@@ -4,7 +4,7 @@ import { getIndividualResults } from '@/queries/functions/getIndividualResults';
 import { QueryKey } from '@/queries/index';
 
 export function useGetIndividualResults({ enabled }: { enabled: boolean }) {
-  return useQuery(QueryKey.GetIndividualResults, getIndividualResults, {
+  return useQuery([QueryKey.GetIndividualResults], getIndividualResults, {
     enabled,
   });
 }
